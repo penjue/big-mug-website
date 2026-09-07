6c9bb67195c7c75d16b4886f80a373a09067c72a
penjue <penjue@gmail.com>
2026-09-07 13:54:59 +0100

    Register SEO admin module

--- policy_admin.py
+++ policy_admin.py
@@ -2,6 +2,9 @@ import html
 from flask import request, redirect, url_for, flash
 import timeline_app as timeline
 
+# Registers editable SEO and social sharing controls.
+import seo_admin
+
 app = timeline.app
 base = timeline.base
 
